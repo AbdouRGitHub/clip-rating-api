aa6e5d1f7fb287244ca3cab064cca5119ad8c6fe
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2024-12-13 00:24:35 +0100

    feat: configure session management and body-parser in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import { TypeormStore } from 'connect-typeorm';
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { Session } from './auth/entities/session.entity';
+import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap(configService: ConfigService) {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const sessionRepository = await app.get(DataSource).getRepository(Session);
+
+  await app.use(
+    session({
+      secret: configService.get('SESSION_SECRET'),
+      resave: false,
+      saveUninitialized: false,
+      name: 'session_nest',
+      store: new TypeormStore().connect(sessionRepository),
+      cookie: {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: configService.get('NODE_ENV') === 'production',
+      },
+    }),
+  );
+  await app.useGlobalPipes(new ValidationPipe());
+  await app.enableCors();
+  await app.use(bodyParser.json({ limit: '100mb' }));
+  await app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap(new ConfigService());
