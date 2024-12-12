12450e7b84516422e9aa85f314d865a33c5d0f0e
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2024-12-13 00:25:21 +0100

    feat: AuthDto validator for email & password

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,11 @@
+import { IsCurrency, IsNotEmpty, IsString } from "class-validator";
+
 export class AuthDto {
+  @IsString()
+  @IsNotEmpty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
