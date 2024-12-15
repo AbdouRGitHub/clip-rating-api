b8d4efa62f36ae3ed25d36232c614b41c3d6fc00
AbdouRGitHub <94540594+AbdouRGitHub@users.noreply.github.com>
2024-12-15 19:57:07 +0100

    feat: add user role to the session data

--- src/auth/entities/session.d.ts
+++ src/auth/entities/session.d.ts
@@ -1,7 +1,9 @@
 import 'express-session';
+import { UserRole } from 'src/user/entities/user.entity';
 
 declare module 'express-session' {
   interface SessionData {
     userId?: string;
+    role?: UserRole;
   }
 }
