cc83aa0606d59a911673955d35dd81c0c7819c43
Abdou <abdousani60@gmail.com>
2025-05-05 17:06:28 +0200

    feat: add moduleNameMapper for path aliasing in Jest configuration and enhance AuthService tests with login functionality

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
+  let userMockRepository = {
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, {
+        provide: getRepositoryToken(User),
+        useValue: userMockRepository,
+      }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { Request } from 'express';
 
 describe('AuthService', () => {
   let service: AuthService;
-
+  let userMockRepository = {
+    findOne: jest.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: userMockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    userMockRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should login with correct parameters', async () => {
+      const authDto = { email: 'test@gmail.com', password: 'test' };
+      const mockRequest = {
+        session: { userId: null },
+      } as any as Request;
+      
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true as never);
+
+      userMockRepository.findOne = jest.fn().mockResolvedValue({
+        id: 1,
+        email: 'test@gmail.com',
+        password: '$2b$10$abcdeHashedPasswordHere',
+      });
+
+      const result = await service.login(authDto, mockRequest);
+      expect(result).toBe(undefined);
+      expect(mockRequest.session.userId).toBe(1);
+    });
   });
 });
